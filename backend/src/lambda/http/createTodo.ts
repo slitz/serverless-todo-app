ba7cc032e21a8e520aa2d78bbe2cbfe25486387f
Scott Litz <scott.litz@gmail.com>
2019-08-05 16:44:56 -0700

    added logic to update existing todo item

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,13 +1,42 @@
-/* import 'source-map-support/register'
-
+import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+import { parseUserId } from '../../auth/utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const itemId = uuid.v4()
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const newItem = {
+    id: itemId,
+    userId: parseUserId(jwtToken),
+    ...newTodo
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
- */
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,14 +1,69 @@
-/* import 'source-map-support/register'
-
+import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const validTodoId = await todoExists(todoId)
+
+  if (!validTodoId) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  var params = {
+    TableName: todosTable,
+    Key: {
+      id: todoId
+    },
+    UpdateExpression: "SET name = :name, dueDate = :dueDate, done = :done",
+    ExpressionAttributeValues:{
+        ":name": updatedTodo.name,
+        ":dueDate": updatedTodo.dueDate,
+        ":done": updatedTodo.done
+    },
+    ReturnValues:"UPDATED_NEW"
+  }
+  
+  await docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  }).promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: null
+  }
 }
- */
+
+async function todoExists(todoId: string) {
+  const result = await docClient
+    .get({
+      TableName: todosTable,
+      Key: {
+        id: todoId
+      }
+    })
+    .promise()
+
+  console.log('Get group: ', result)
+  return !!result.Item
+}
