11037c7e9c8ad4a86326dc2f70add7b75e7983a8
Scott Litz <scott.litz@gmail.com>
2019-08-07 14:46:51 -0700

    added generateUploadUrl logic

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,45 @@
-/* import 'source-map-support/register'
-
+import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
+import { todoExists } from '../utils';
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const todosTable = process.env.TODOS_TABLE
+const attachmentsBucket = process.env.ATTACHMENTS_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const validTodoId = await todoExists(todoId, docClient, todosTable)
 
+  if (!validTodoId) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+  
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const url = getUploadUrl(todoId)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
 }
- */
+
+function getUploadUrl(todoId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: attachmentsBucket,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+}
