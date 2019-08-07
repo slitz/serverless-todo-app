c1a0709e20e224cb4c8662b0f6f7fb5caa33ee31
Scott Litz <scott.litz@gmail.com>
2019-08-07 16:50:03 -0700

    changed getTodos logic to query table by userId

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import * as AWS  from 'aws-sdk'
+import { getUserId } from '../utils';
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -11,12 +12,16 @@ const todosTable = process.env.TODOS_TABLE
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   console.log('Processing event: ', event)
+  const userId = getUserId(event)
 
-  const result = await docClient.scan({
-    TableName: todosTable
+  const result = await docClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
   }).promise()
-
-  const items = result.Items
 
   return {
     statusCode: 200,
@@ -24,7 +29,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items
+      result
     })
   }
 }
